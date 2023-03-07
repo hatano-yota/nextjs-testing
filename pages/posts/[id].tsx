bf862b0cd95efb83388a8f03983daebf7455feb3
unreact-hatano <mizutama.hahaha@gmail.com>
2023-03-07 14:24:59 +0900

    ブログページの実装 #5
    interfaceの設定
    fetchの関数を追加(4つ)
    getStaticProps、getStaticPathsを適用して
    blog-page、post/[id]ページを実装

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+import Layout from '../../components/Layout'
+import { getAllPostsIds, getPostData } from '../../lib/fetch'
+import { POST } from '../../types/Types'
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+const PostDetail: React.FC<POST> = ({ id, title, body }) => {
+  return (
+    <Layout title={title}>
+      <p className="mt-4">
+        {'ID : '}
+        {id}
+      </p>
+      <p className="mb-4 text-xl font-bold">{title}</p>
+      <p className="mx-10 mb-12">{body}</p>
+      <Link href="/blog-page">
+        <div className="flex cursor-pointer mt-12 hover:bg-gray-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 mr-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+            />
+          </svg>
+          <a data-testId="back-blog">Back to blog-page</a>
+        </div>
+      </Link>
+    </Layout>
+  )
+}
+
+export default PostDetail
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getAllPostsIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const post = await getPostData(ctx.params.id as string)
+  return {
+    props: {
+      ...post,
+    },
+  }
+}
