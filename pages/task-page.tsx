2e9b89ca774f7062d8c452d4cf7c9faaef03fc96
unreact-hatano <mizutama.hahaha@gmail.com>
2023-04-10 16:16:09 +0900

    fix error msg

--- pages/task-page.tsx
+++ pages/task-page.tsx
@@ -21,7 +21,7 @@ const TaskPage: React.FC<STATICPROPS> = ({ staticTasks }) => {
     fallbackData: staticTasks,
     revalidateOnMount: true,
   })
-  if (error) return <span>Erro!</span>
+  if (error) return <span>Error!</span>
 
   return (
     <Layout title="Todos">
